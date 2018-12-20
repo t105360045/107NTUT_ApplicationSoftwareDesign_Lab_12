05ea5613444bfc4a80572e1c8be8d0f34a89b9c8
Your Name <you@example.com>
2018-12-20 14:31:22 +0800

    finish

---
+++ app/src/main/java/com/example/user/lab12/Data.java
@@ -0,0 +1,12 @@
+package com.example.user.lab12;
+
+class Data{
+    Result result;
+    class Result{
+        Results[] results;
+        class Results{
+            String Station;
+            String Destination;
+        }
+    }
+}
